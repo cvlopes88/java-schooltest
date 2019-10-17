04d0cc8b3cae643e5187cb82cfdcba772a367452
Luis Mendes <cvlopes88@gmail.com>
2019-10-16 22:00:46 -0400

    SMore

--- school/src/test/java/com/lambdaschool/school/controller/CourseControllerTest.java
+++ school/src/test/java/com/lambdaschool/school/controller/CourseControllerTest.java
@@ -1,17 +1,26 @@
 package com.lambdaschool.school.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.lambdaschool.school.model.Course;
+import com.lambdaschool.school.model.Instructor;
 import com.lambdaschool.school.service.CourseService;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
+import java.awt.*;
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.Assert.*;
@@ -24,11 +33,22 @@ public class CourseControllerTest {
 
     @MockBean
     private CourseService courseService;
-    private List<Course> courseList;
+    private ArrayList<Course> courseList;
 
     
     @Before
     public void setUp() throws Exception {
+
+        courseList = new ArrayList<>();
+        Course a = new Course("java", new Instructor("carlos"));
+        a.setCourseid(1L);
+
+        Course b = new Course("react", new Instructor("pedro"));
+        b.setCourseid(2L);
+
+        courseList.add(a);
+        courseList.add(b);
+
     }
 
     @After
@@ -40,7 +60,18 @@ public class CourseControllerTest {
     }
 
     @Test
-    public void listAllCourses() {
+    public void listAllCourses() throws Exception {
+        String apiUrl = "/Courses/Courses";
+        Mockito.when(courseService.findAll()).thenReturn(courseList);
+
+        RequestBuilder rb = MockMvcRequestBuilders.get(apiUrl).accept(MediaType.APPLICATION_JSON);
+        MvcResult r = mockMvc.perform(rb).andReturn();
+        String tr = r.getResponse().getContentAsString();
+        ObjectMapper mapper = new ObjectMapper();
+        String er = mapper.writeValueAsString(courseList);
+
+        assertEquals("Rest api returns all", er, tr);
+
     }
 
     @Test
